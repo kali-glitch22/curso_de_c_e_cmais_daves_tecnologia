79eba84191c58c89c9d62ef48fd5fcbff22ab5a9
kali-glitch <73629043+kali-glitch22@users.noreply.github.com>
2021-03-29 10:22:50 -0300

    Update 7.cpp

--- 7.cpp
+++ 7.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
     int numero;
     int j; 
-    for ( numero=0; numero<=100000; numero++){
+    for ( numero=0; numero<=100; numero++){
         cout << "do you know da wae";
         
         for (j= 0; j<1;j++){
